cd03b40617334074562f0bf0710052dfeedff847
Vasylyna Andybur <vasylyna1206@yahoo.com>
2021-07-13 00:56:07 +0300

    two additional tasks done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 
-
+//Color switcher
     const pageBackground= document.querySelector('body')
     const startButton = document.querySelector('[data-start]')
     const stopButton = document.querySelector('[data-stop]')
@@ -33,3 +33,52 @@ function stopChangeBgColor() {
     clearInterval(intervalId);
 
 }
+
+
+
+
+
+//delay function
+
+const delay = ms => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
+};
+
+const logger = time => console.log(`Fulfilled after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Fulfilled after 2000ms
+delay(1000).then(logger); // Fulfilled after 1000ms
+delay(1500).then(logger); // Fulfilled after 1500ms
+
+
+//toggleUserState function
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: false },
+];
+
+const toggleUserState = (allUsers, username,) => {
+    return new Promise((resolve, reject) => {
+        const updatedUsers = allUsers.map(user =>
+            user.name === username ? { ...user, active: !user.active } : user
+        );
+        resolve(updatedUsers);
+    });
+  
+};
+const result =  updatedUsers => console.table(updatedUsers);
+
+
+// Currently the function works like this
+// toggleUserState(users, 'Mango', console.table);
+// toggleUserState(users, 'Ajax', console.table);
+
+// The function should work like this
+toggleUserState(users, 'Mango').then(console.table);
+toggleUserState(users, 'Poly').then(console.table);
