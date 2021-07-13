b8c7abc542eb7e64d8adc5c7be8f7cf4ddd34747
Vasylyna Andybur <vasylyna1206@yahoo.com>
2021-07-13 23:50:25 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './sass/main.scss';
-// import './js-files/color-switcher'
+import './js-files/color-switcher'
 import './js-files/timer'
-// import './js-files/delay-fn'
-// import './js-files/user-state-fn'
-// import './js-files/transaction'
+import './js-files/delay-fn'
+import './js-files/user-state-fn'
+import './js-files/transaction'
 
 
 
